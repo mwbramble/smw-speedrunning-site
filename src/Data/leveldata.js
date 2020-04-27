78aa2426fa943f5587389c1ac6913e20e6768528
Michael Bramble <mike.w.bramble@gmail.com>
2020-04-27 03:00:59 -0400

    started adding level data....this will take a while

--- src/Data/leveldata.js
+++ src/Data/leveldata.js
@@ -1,34 +1,366 @@
-// Level Template
-// {
-//   name: ``,
-//   id: '',
-//   world: ,
-//   image: '',
-//   powerups: [],
-//   dragoncoins: ,
-//   categories: [
-//      {
-//        title: ``,
-//        exits: {
-//          normal: {
-//            strats: [
-//              {
-//                id: ,
-//                name: ``,
-//                time: ,
-//                link: '',
-//                desc: ``,
-//                source: ``
-//              }
-//            ]
-//          },
-//          secret: {}
-//        }
-//      },
-//   ]
-// },
-
 export default [
+  { // Awesome
+    name: `Awesome`,
+    id: '12D',
+    world: 9,
+    image: null,
+    powerups: ['Fire'],
+    dragoncoins: true,
+    categories: [
+      {
+        title: `96 Exit`,
+        exits: {
+          normal: {
+            strats: [
+              {
+                id: 1,
+                name: `ARK Tutorial`,
+                time: null,
+                link: 'https://www.youtube.com/embed/Y4yU0bFXq_A',
+                desc: ``,
+                source: `Bramz`,
+              },
+              {
+                id: 2,
+                name: `Flower Grab`,
+                time: 261,
+                link: 'https://www.youtube.com/embed/sFttmNb8SZ8',
+                desc: `This is the fastest flower to grab in Special World, and you'll need it if you go for Groovy Boss Kill.`,
+                source: `Sten`,
+              },
+              {
+                id: 3,
+                name: `Normal Strat`,
+                time: 261,
+                link: 'https://www.youtube.com/embed/_plp5rw82JE',
+                desc: `If you don't go for Groovy Boss Kill and you're struggling with the flower grab strat, you can do this and get flower in Groovy instead.`,
+                source: `Sten`,
+              },
+            ],
+          },
+          secret: null,
+        },
+      },
+      {
+        title: `95 Exit, No Cape`,
+        exits: {
+          normal: {
+            strats: [
+              {
+                id: 4,
+                name: `No Cape, Blue Yoshi`,
+                time: 260,
+                link: 'https://www.youtube.com/embed/8HFfU7xLL0w',
+                desc: `Basically the same as the strat for 96 Exit, but of course there's no cape.`,
+                source: `Sten`,
+              },
+            ],
+          },
+          secret: null,
+        },
+      },
+      {
+        title: `Lunar Dragon`,
+        exits: {
+          normal: {
+            strats: [
+              {
+                id: 5,
+                name: `Flower Grab`,
+                time: 261,
+                link: 'https://www.youtube.com/embed/AXKNH-4HUgQ',
+                desc: `This is the fastest flower grab. If you struggle with this, you can skip it and get a flower in Groovy or Vanilla Dome 1.`,
+                source: `Umari0`,
+              },
+            ],
+          },
+          secret: null,
+        },
+      },
+    ],
+  },
+  { // Blue Switch Palace
+    name: `Blue Switch Palace`,
+    id: '121',
+    world: 5,
+    image: null,
+    powerups: ['None'],
+    dragoncoins: false,
+    categories: [
+      {
+        title: `96 Exit`,
+        exits: {
+          normal: {
+            strats: [
+              {
+                id: 1,
+                name: `Pipe Fly`,
+                time: 190,
+                link: 'https://www.youtube.com/embed/SSS-m-q8s8w',
+                desc: `Alternatively, you can skip the pipe fly. This is a bit slower.`,
+                source: `Sten`,
+              },
+            ],
+          },
+          secret: null,
+        },
+      },
+      {
+        title: `95 Exit, No Cape`,
+        exits: {
+          normal: {
+            strats: [
+              {
+                id: 2,
+                name: `No Cape`,
+                time: 189,
+                link: 'https://www.youtube.com/embed/mg1b7WTVzA4',
+                desc: ``,
+                source: `Sten`
+              },
+            ]
+          },
+          secret: null
+        }
+      },
+      {
+        title: `Lunar Dragon`,
+        exits: {
+          normal: {
+            strats: [
+              {
+                id: 3,
+                name: `Pipe Fly`,
+                time: 190,
+                link: 'https://www.youtube.com/embed/SSS-m-q8s8w',
+                desc: `Alternatively, you can skip the pipe fly. This is a bit slower.`,
+                source: `Sten`,
+              },
+            ],
+          },
+          secret: null,
+        },
+      },
+    ],
+  },
+  { // Butter Bridge 1
+    name: `Butter Bridge 1`,
+    id: 'C',
+    world: 4,
+    image: null,
+    powerups: ['Cape'],
+    dragoncoins: true,
+    categories: [
+      {
+        title: `96 Exit`,
+        exits: {
+          normal: {
+            strats: [
+              {
+                id: 1,
+                name: `Pipe Fly`,
+                time: 154,
+                link: 'https://www.youtube.com/embed/gJwfcH_xj30',
+                desc: `This setup avoids spinning any parakoopas. Alternatively, you can skip the pipe fly.`,
+                source: `Lui`
+              },
+             ]
+           },
+           secret: null
+         }
+      },
+      {
+        title: `No Starworld`,
+        exits: {
+          normal: {
+            strats: [
+             {
+               id: 2,
+               name: `Pipe Fly`,
+               time: 154,
+               link: 'https://www.youtube.com/embed/gJwfcH_xj30',
+               desc: `This setup avoids spinning any parakoopas. Alternatively, you can skip the pipe fly.`,
+               source: `Lui`
+             },
+            ]
+          },
+          secret: null
+        }
+      },
+      {
+        title: `95 Exit, No Cape`,
+        exits: {
+          normal: {
+            strats: [
+             {
+               id: 3,
+               name: `No Cape`,
+               time: 153,
+               link: 'https://www.youtube.com/embed/dy6hYkH3wuU',
+               desc: ``,
+               source: `Sten`
+             },
+            ]
+          },
+          secret: null
+        }
+      },
+      {
+        title: `No Cape, No Starworld`,
+        exits: {
+          normal: {
+            strats: [
+             {
+               id: 4,
+               name: `No Cape`,
+               time: 153,
+               link: 'https://www.youtube.com/embed/dy6hYkH3wuU',
+               desc: ``,
+               source: `Sten`
+             },
+            ]
+          },
+          secret: null
+        }
+      },
+      {
+        title: `Lunar Dragon`,
+        exits: {
+          normal: {
+            strats: [
+             {
+               id: 5,
+               name: `No Cape`,
+               time: 154,
+               link: 'https://www.youtube.com/embed/Q986GKw59Ig',
+               desc: ``,
+               source: `Umari0`
+             },
+            ]
+          },
+          secret: null
+        }
+      },
+    ]
+  },
+  { // Butter Bridge 2
+    name: `Butter Bridge 2`,
+    id: 'D',
+    world: 4,
+    image: null,
+    powerups: ['Cape', 'Yoshi'],
+    dragoncoins: true,
+    categories: [
+      {
+        title: `96 Exit`,
+        exits: {
+          normal: {
+            strats: [
+              {
+                id: 1,
+                name: `Normal Strat`,
+                time: 263,
+                link: 'https://www.youtube.com/embed/GvamBJKhUqE',
+                desc: ``,
+                source: `Bramz`
+              },
+            ]
+          },
+          secret: null
+        }
+      },
+      {
+        title: `No Starworld`,
+        exits: {
+          normal: {
+            strats: [
+              {
+                id: 2,
+                name: `Normal Strat`,
+                time: 263,
+                link: 'https://www.youtube.com/embed/GvamBJKhUqE',
+                desc: ``,
+                source: `Bramz`
+              },
+            ]
+          },
+          secret: null
+        }
+      },
+      {
+        title: `95 Exit, No Cape`,
+        exits: {
+          normal: {
+            strats: [
+              {
+                id: 3,
+                name: `No Cape`,
+                time: 261,
+                link: 'https://www.youtube.com/embed/-ky6Uthz5fU',
+                desc: ``,
+                source: `Sten`
+              },
+            ]
+          },
+          secret: null
+        }
+      },
+      {
+        title: `No Cape, No Starworld`,
+        exits: {
+          normal: {
+            strats: [
+              {
+                id: 4,
+                name: `No Cape`,
+                time: 261,
+                link: 'https://www.youtube.com/embed/-ky6Uthz5fU',
+                desc: ``,
+                source: `Sten`
+              },
+            ]
+          },
+          secret: null
+        }
+      },
+      {
+        title: `No Starworld, Small Only`,
+        exits: {
+          normal: {
+            strats: [
+              {
+                id: 5,
+                name: `Small Only`,
+                time: 260,
+                link: 'https://www.youtube.com/embed/xs-MrooJXys',
+                desc: ``,
+                source: `rezephos`
+              },
+            ]
+          },
+          secret: null
+        }
+      },
+      {
+        title: `Lunar Dragon`,
+        exits: {
+          normal: {
+            strats: [
+              {
+                id: 6,
+                name: `Lunar Dragon`,
+                time: 250,
+                link: 'https://www.youtube.com/embed/xxhg7JY_fiE',
+                desc: ``,
+                source: `Umari0`
+              },
+            ]
+          },
+          secret: null
+        }
+      },
+    ]
+  },
   {
     name: `Vanilla Dome 1`,
     id: '11A',
@@ -36,172 +368,171 @@ export default [
     image: null,
     powerups: ['Fire', 'Cape (NTSC-U)'],
     dragoncoins: true,
-    categories: 
-      [
-        {
-          title: `96 Exit`,
-          id: `96_exit`,
-          exits: {
-            normal: {
-              strats: [
-                {
-                  id: 1,
-                  name: `Wings Tutorial`,
-                  time: null,
-                  link: 'https://www.youtube.com/embed/s6-TlVgmNHQ',
-                  desc: `One thing not mentioned in this tutorial is the possibility of hitting the key block. This can happen by picking up the Buzzy Beetle while it has horizonal momentum, or by going for the second dupe while the block beneath the invisible key block is spinning.`,
-                  source: `Bramz`,
-                },
-                {
-                  id: 2,
-                  name: `Fast Wings`,
-                  time: 378,
-                  link: 'https://www.youtube.com/embed/wT9b2scXB9I',
-                  desc: `This is not recommended for beginners.`,
-                  source: `Lui`,
-                },
-                {
-                  id: 3,
-                  name: `Intermediate Wings`,
-                  time: 375,
-                  link: 'https://www.youtube.com/embed/Pauba8wh9MA',
-                  desc: ``,
-                  source: `Lui`
-                },
-                {
-                  id: 4,
-                  name: `Easy`,
-                  time: 372,
-                  link: 'https://www.youtube.com/embed/BRiMrDl9y4U',
-                  desc: ``,
-                  source: `Sten`
-                },
-                {
-                  id: 5,
-                  name: `No Wings`,
-                  time: 343,
-                  link: 'https://www.youtube.com/embed/pghNLJ3tqlM',
-                  desc: `Use this strat if you aren't going for wings just yet.`,
-                  source: `Sten`
-                },
-              ]
-            },
-            secret: {
-              strats: [
-                {
-                  id: 6,
-                  name: `Normal Strat`,
-                  time: 382,
-                  link: 'https://www.youtube.com/embed/ovyIEN6PHqc',
-                  desc: ``,
-                  source: `Sten`
-                },
-                {
-                  id: 7,
-                  name: `Fast Strat`,
-                  time: 383,
-                  link: 'https://www.youtube.com/embed/cxCzl9Xls3E',
-                  desc: `The turnaround here is very precise, and it is more likely that you will lose time doing it.`,
-                  source: `LostC0re`
-                },
-              ]
-            }
-          }
+    categories: [
+      {
+        title: `96 Exit`,
+        id: `96_exit`,
+        exits: {
+          normal: {
+            strats: [
+              {
+                id: 1,
+                name: `Wings Tutorial`,
+                time: null,
+                link: 'https://www.youtube.com/embed/s6-TlVgmNHQ',
+                desc: `One thing not mentioned in this tutorial is the possibility of hitting the key block. This can happen by picking up the Buzzy Beetle while it has horizonal momentum, or by going for the second dupe while the block beneath the invisible key block is spinning.`,
+                source: `Bramz`,
+              },
+              {
+                id: 2,
+                name: `Fast Wings`,
+                time: 378,
+                link: 'https://www.youtube.com/embed/wT9b2scXB9I',
+                desc: `This is not recommended for beginners.`,
+                source: `Lui`,
+              },
+              {
+                id: 3,
+                name: `Intermediate Wings`,
+                time: 375,
+                link: 'https://www.youtube.com/embed/Pauba8wh9MA',
+                desc: ``,
+                source: `Lui`,
+              },
+              {
+                id: 4,
+                name: `Easy`,
+                time: 372,
+                link: 'https://www.youtube.com/embed/BRiMrDl9y4U',
+                desc: ``,
+                source: `Sten`,
+              },
+              {
+                id: 5,
+                name: `No Wings`,
+                time: 343,
+                link: 'https://www.youtube.com/embed/pghNLJ3tqlM',
+                desc: `Use this strat if you aren't going for wings just yet.`,
+                source: `Sten`,
+              },
+            ],
+          },
+          secret: {
+            strats: [
+              {
+                id: 6,
+                name: `Normal Strat`,
+                time: 382,
+                link: 'https://www.youtube.com/embed/ovyIEN6PHqc',
+                desc: ``,
+                source: `Sten`,
+              },
+              {
+                id: 7,
+                name: `Fast Strat`,
+                time: 383,
+                link: 'https://www.youtube.com/embed/cxCzl9Xls3E',
+                desc: `The turnaround here is very precise, and it is more likely that you will lose time doing it.`,
+                source: `LostC0re`,
+              },
+            ],
+          },
         },
-        {
-          title: `All Castles`,
-          id: 'ac',
-          exits: {
-            normal: {
-              strats: []
-            },
-            secret: null
-          }
+      },
+      {
+        title: `All Castles`,
+        id: 'ac',
+        exits: {
+          normal: {
+            strats: [],
+          },
+          secret: null,
         },
-        {
-          title: `No Starworld`,
-          id: 'nsw',
-          exits: {
-            normal: null,
-            secret: {
-              strats: []
-            }
-          }
+      },
+      {
+        title: `No Starworld`,
+        id: 'nsw',
+        exits: {
+          normal: null,
+          secret: {
+            strats: [],
+          },
         },
-        {
-          title: `95 Exit, No Cape`,
-          id: '95nc',
-          exits: {
-            normal: {
-              strats: []
-            },
-            secret: {
-              strats: []
-            }
-          }
+      },
+      {
+        title: `95 Exit, No Cape`,
+        id: '95nc',
+        exits: {
+          normal: {
+            strats: [],
+          },
+          secret: {
+            strats: [],
+          },
         },
-        {
-          title: `All Castles, No Cape`,
-          id: 'acnc',
-          exits: {
-            normal: {
-              strats: []
-            },
-            secret: null
-          }
+      },
+      {
+        title: `All Castles, No Cape`,
+        id: 'acnc',
+        exits: {
+          normal: {
+            strats: [],
+          },
+          secret: null,
         },
-        {
-          title: `No Cape, No Starworld`,
-          id: 'ncnsw',
-          exits: {
-            normal: null,
-            secret: {
-              strats: []
-            }
-          }
+      },
+      {
+        title: `No Cape, No Starworld`,
+        id: 'ncnsw',
+        exits: {
+          normal: null,
+          secret: {
+            strats: [],
+          },
         },
-        {
-          title: `All Castles, Small Only`,
-          id: 'acso',
-          exits: {
-            normal: {
-              strats: []
-            },
-            secret: null
-          }
+      },
+      {
+        title: `All Castles, Small Only`,
+        id: 'acso',
+        exits: {
+          normal: {
+            strats: [],
+          },
+          secret: null,
         },
-        {
-          title: `No Starworld, Small Only`,
-          id: 'nswso',
-          exits: {
-            normal: null,
-            secret: {
-              strats: []
-            }
-          }
+      },
+      {
+        title: `No Starworld, Small Only`,
+        id: 'nswso',
+        exits: {
+          normal: null,
+          secret: {
+            strats: [],
+          },
         },
-        {
-          title: `Small Only`,
-          id: 'small',
-          exits: {
-            normal: null,
-            secret: {
-              strats: []
-            },
-          }
+      },
+      {
+        title: `Small Only`,
+        id: 'small',
+        exits: {
+          normal: null,
+          secret: {
+            strats: [],
+          },
         },
-        {
-          title: `Lunar Dragon`,
-          id: 'lunar_d',
-          exits: {
-            normal: {
-              strats: []
-            },
-            secret: {
-              strats: []
-            }
-          }
+      },
+      {
+        title: `Lunar Dragon`,
+        id: 'lunar_d',
+        exits: {
+          normal: {
+            strats: [],
+          },
+          secret: {
+            strats: [],
+          },
         },
-      ]
+      },
+    ],
   },
-]
+];
---
+++ src/Data/leveldataformats.js
@@ -0,0 +1,48 @@
+// Category Order
+// 96, AC, NSW, 11, 0, 95NC, ACNC, NCNSW, 11NC, ACSO, NSWSO, Small, LD
+
+// Level Template
+{
+  name: ``,
+  id: '',
+  world: ,
+  image: null,
+  powerups: [],
+  dragoncoins: ,
+  categories: [
+    {
+      title: ``,
+      exits: {
+        normal: {
+          strats: []
+        },
+        secret: {
+          strats: []
+        }
+      }
+    },
+  ]
+},
+
+// Category Template
+{
+  title: ``,
+  exits: {
+    normal: {
+      strats: []
+    },
+    secret: {
+      strats: []
+    }
+  }
+},
+
+// Strat Template
+{
+	id: ,
+	name: ``,
+	time: ,
+	link: '',
+	desc: ``,
+	source: ``
+},
