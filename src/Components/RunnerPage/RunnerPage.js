232be78f5e207c0d0966cb5c3e63edabd8b869ed
Michael Bramble <mike.w.bramble@gmail.com>
2020-04-29 02:42:34 -0400

    added all runner data and flag images

--- src/Components/RunnerPage/RunnerPage.js
+++ src/Components/RunnerPage/RunnerPage.js
@@ -3,24 +3,65 @@ import RUNNERS from '../../Data/runnerdata';
 import './runnerpage.css';
 
 export default class RunnerPage extends Component {
+  findFlag(country){
+    if(country === 'Australia'){
+      return <img id='runner-flag' src={require('../../Images/Flags/Australia.png')} alt='Australia Flag' />
+    }
+    if(country === 'Brazil'){
+      return <img id='runner-flag' src={require('../../Images/Flags/Brazil.png')} alt='Brazil Flag' />
+    }
+    if(country ==='Canada'){
+      return <img id='runner-flag' src={require('../../Images/Flags/Canada.png')} alt='Canada Flag' />
+    }
+    if(country ==='Chile'){
+      return <img id='runner-flag' src={require('../../Images/Flags/Chile.png')} alt='Chile Flag' />
+    }
+    if(country ==='England'){
+      return <img id='runner-flag' src={require('../../Images/Flags/England.png')} alt='England Flag' />
+    }
+    if(country ==='France'){
+      return <img id='runner-flag' src={require('../../Images/Flags/France.png')} alt='France Flag' />
+    }
+    if(country ==='Germany'){
+      return <img id='runner-flag' src={require('../../Images/Flags/Germany.png')} alt='Germany Flag' />
+    }
+    if(country ==='Ireland'){
+      return <img id='runner-flag' src={require('../../Images/Flags/Ireland.png')} alt='Ireland Flag' />
+    }
+    if(country ==='Italy'){
+      return <img id='runner-flag' src={require('../../Images/Flags/Italy.png')} alt='Italy Flag' />
+    }
+    if(country ==='Japan'){
+      return <img id='runner-flag' src={require('../../Images/Flags/Japan.png')} alt='Japan Flag' />
+    }
+    if(country === 'Sweden'){
+      return <img id='runner-flag' src={require('../../Images/Flags/Sweden.png')} alt='Sweden Flag' />
+    }
+    if(country === 'USA'){
+      return <img id='runner-flag' src={require('../../Images/Flags/USA.png')} alt='USA Flag' />
+    }
+    else return '';
+  }
+
   render(){
-    const runner = RUNNERS.filter(r => r.id === this.props.match.params.id);
-    const twitch = `https://twitch.tv/${runner[0].links.twitch}`;
-    const youtube = `https://youtube.com/${runner[0].links.youtube}`;
-    const twitter = `https://twitter.com/${runner[0].links.twitter}`;
-    const srcom = `https://speedrun.com/${runner[0].links.srcom}`;
+    const r = RUNNERS.filter(r => r.id === this.props.match.params.id)[0];
+    const twitch = `https://twitch.tv/${r.links.twitch}`;
+    const youtube = `https://youtube.com/${r.links.youtube}`;
+    const twitter = `https://twitter.com/${r.links.twitter}`;
+    const srcom = `https://speedrun.com/${r.links.srcom}`;
+    
     return (
       <section className='runnerpage-container'>
-        <h1>{runner[0].name}</h1>
-        <h3 id='location'>{runner[0].location.subcountry}, {runner[0].location.country} | Runner Since {runner[0].began}</h3>
+        <h1>{r.name}</h1>
+        <h3 id='location'>{r.location.subcountry ? r.location.subcountry + ', ' : ''}{r.location.country ? r.location.country : ''} {this.findFlag(r.location.country)} | Runner Since {r.began}</h3>
         <ul id='socials'>
-          <li><a href={twitch}>Twitch</a></li>
-          <li><a href={youtube}>YouTube</a></li>
-          <li><a href={twitter}>Twitter</a></li>
-          <li>Discord: @{runner[0].links.discord}</li>
-          <li><a href={srcom}>speedrun.com</a></li>
+          {r.links.twitch ? <li><a href={twitch}>Twitch</a></li> : ''}
+          {r.links.youtube ? <li><a href={youtube}>YouTube</a></li> : ''}
+          {r.links.twitter ? <li><a href={twitter}>Twitter</a></li> : ''}
+          {r.links.discord ? <li>Discord: @{r.links.discord}</li> : ''}
+          {r.links.srcom ? <li><a href={srcom}>speedrun.com</a></li> : ''}
         </ul>
-        <p id='bio'>{runner[0].bio}</p>
+        <p id='bio'>{r.bio}</p>
       </section>
     )
   }
--- src/Components/Runners/Runners.js
+++ src/Components/Runners/Runners.js
@@ -4,18 +4,75 @@ import RUNNERS from '../../Data/runnerdata';
 import './runners.css';
 
 export default class Runners extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      display: 'all'
+    }
+  }
+  
+  sortRunners(){
+    let opt = document.getElementById('runner-type').value;
+    this.setState({display: opt});
+  }
+  
   render(){
     return (
       <section className='runners-container'>
         <h1>Runners</h1>
-        <ul className='runnerList'>
-          {RUNNERS.map(runner =>
-            <li key={runner.id}>
-              <Link to={`/runners/${runner.id}`}>
-                {runner.name}
-              </Link>
-            </li>
-          )}
+        <label for='runner-type'>Sort Runners By: </label>
+        <select id='runner-type' onChange={() => this.sortRunners()}>
+          <option value='all'>All</option>
+          <option value='top10'>Top 10 Ranked Runner</option>
+          <option value='lbmod'>Leaderboard Mod</option>
+          <option value='discordmod'>Discord Mod</option>
+        </select>
+        <ul className='runner-list'>
+          {RUNNERS.map(r => {
+            if(this.state.display === 'all'){
+              return (
+                <li key={r.id}>
+                  <Link to={`/runners/${r.id}`}>
+                    {r.name}
+                  </Link>
+                </li>
+              )
+            }
+            if(this.state.display === 'lbmod'){
+              if(r.lbmod){
+                return (
+                  <li key={r.id}>
+                    <Link to={`/runners/${r.id}`}>
+                      {r.name}
+                    </Link>
+                  </li>
+                )
+              }
+            }
+            if(this.state.display === 'discordmod'){
+              if(r.discordmod){
+                return (
+                  <li key={r.id}>
+                    <Link to={`/runners/${r.id}`}>
+                      {r.name}
+                    </Link>
+                  </li>
+                )
+              }
+            }
+            if(this.state.display === 'top10'){
+              if(r.top10){
+                return (
+                  <li key={r.id}>
+                    <Link to={`/runners/${r.id}`}>
+                      {r.name}
+                    </Link>
+                  </li>
+                )
+              }
+            }
+            return null;
+          })}
         </ul>
       </section>
     )
