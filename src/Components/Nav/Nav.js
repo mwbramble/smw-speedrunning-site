f3cadb9527b6249b1617fa0c938b7a8d5c80a791
Michael Bramble <mike.w.bramble@gmail.com>
2021-02-05 13:06:23 -0500

    fixed a bug in the nav bar, and changed icon sizes on the dark mode toggle

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -39,36 +39,24 @@ export default class Nav extends Component{
         <Link to={'/'}><img src={Seal} id='seal' alt='SMW Seal' /></Link>
         <h1 id='title'>SMW Speedrunning Info</h1>
         <ul className='nav-link-list'>
-          <li>
-            <NavLink to={'/levels'} className='nav-link'>
-              Levels
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={'/categories'} className='nav-link'>
-              Categories
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={'/tutorials'} className='nav-link'>
-              Tutorials
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={'/glossary'} className='nav-link'>
-              Glossary
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={'/runners'} className='nav-link'>
-              Runners
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={'/contact'} className='nav-link'>
-              Contact
-            </NavLink>
-          </li>
+          <NavLink to={'/levels'} className='nav-link'>
+            <li>Levels</li>
+          </NavLink>
+          <NavLink to={'/categories'} className='nav-link'>
+            <li>Categories</li>
+          </NavLink>
+          <NavLink to={'/tutorials'} className='nav-link'>
+            <li>Tutorials</li>
+          </NavLink>
+          <NavLink to={'/glossary'} className='nav-link'>
+            <li>Glossary</li>
+          </NavLink>
+          <NavLink to={'/runners'} className='nav-link'>
+            <li>Runners</li>
+          </NavLink>
+          <NavLink to={'/contact'} className='nav-link'>
+            <li>Contact</li>
+          </NavLink>
         </ul>
         <div className='theme-switch-container'>
           <img src={Star} id='theme-icon' alt='Star' />
