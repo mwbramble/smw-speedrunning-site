9d120cb07fd41cfc1f581a82ddab6dc94baf1bff
Michael Bramble <mike.w.bramble@gmail.com>
2021-02-05 12:41:00 -0500

    styled the levels page

--- src/Components/Levels/Levels.js
+++ src/Components/Levels/Levels.js
@@ -8,141 +8,161 @@ export default class Levels extends Component{
     return (
       <section className='levels-container'>
         <h1 id='page-title'>Levels</h1>
-        <h3>World 1</h3>
-        <ul className='world-list' id='world-1'>
-          {LEVELS.map(l => {
-            if(l.world === 1){
-              return (
-                <li key={l.id}>
-                  <Link to={`/levels/${l.id}`}>
-                    {l.name}
-                  </Link>
-                </li>
-              )
-            }
-            return null;
-          })}
-        </ul>
-        <h3>World 2</h3>
-        <ul className='world-list' id='world-2'>
-          {LEVELS.map(l => {
-            if(l.world === 2){
-              return (
-                <li key={l.id}>
-                  <Link to={`/levels/${l.id}`}>
-                    {l.name}
-                  </Link>
-                </li>
-              )
-            }
-            return null;
-          })}
-        </ul>
-        <h3>World 3</h3>
-        <ul className='world-list' id='world-3'>
-          {LEVELS.map(l => {
-            if(l.world === 3){
-              return (
-                <li key={l.id}>
-                  <Link to={`/levels/${l.id}`}>
-                    {l.name}
-                  </Link>
-                </li>
-              )
-            }
-            return null;
-          })}
-        </ul>
-        <h3>World 4</h3>
-        <ul className='world-list' id='world-4'>
-          {LEVELS.map(l => {
-            if(l.world === 4){
-              return (
-                <li key={l.id}>
-                  <Link to={`/levels/${l.id}`}>
-                    {l.name}
-                  </Link>
-                </li>
-              )
-            }
-            return null;
-          })}
-        </ul>
-        <h3>World 5</h3>
-        <ul className='world-list' id='world-5'>
-          {LEVELS.map(l => {
-            if(l.world === 5){
-              return (
-                <li key={l.id}>
-                  <Link to={`/levels/${l.id}`}>
-                    {l.name}
-                  </Link>
-                </li>
-              )
-            }
-            return null;
-          })}
-        </ul>
-        <h3>World 6</h3>
-        <ul className='world-list' id='world-6'>
-          {LEVELS.map(l => {
-            if(l.world === 6){
-              return (
-                <li key={l.id}>
-                  <Link to={`/levels/${l.id}`}>
-                    {l.name}
-                  </Link>
-                </li>
-              )
-            }
-            return null;
-          })}
-        </ul>
-        <h3>World 7</h3>
-        <ul className='world-list' id='world-7'>
-          {LEVELS.map(l => {
-            if(l.world === 7){
-              return (
-                <li key={l.id}>
-                  <Link to={`/levels/${l.id}`}>
-                    {l.name}
-                  </Link>
-                </li>
-              )
-            }
-            return null;
-          })}
-        </ul>
-        <h3>Star World</h3>
-        <ul className='world-list' id='world-8'>
-          {LEVELS.map(l => {
-            if(l.world === 8){
-              return (
-                <li key={l.id}>
-                  <Link to={`/levels/${l.id}`}>
-                    {l.name}
-                  </Link>
-                </li>
-              )
-            }
-            return null;
-          })}
-        </ul>
-        <h3>Special World</h3>
-        <ul className='world-list' id='world-9'>
-          {LEVELS.map(l => {
-            if(l.world === 9){
-              return (
-                <li key={l.id}>
-                  <Link to={`/levels/${l.id}`}>
-                    {l.name}
-                  </Link>
-                </li>
-              )
-            }
-            return null;
-          })}
-        </ul>
+        <div className='lists-container'>
+          <div className='world-list'>
+            <h3 id='world-name'>World 1</h3>
+            <ul id='world-1'>
+              {LEVELS.map(l => {
+                if(l.world === 1){
+                  return (
+                    <li key={l.id}>
+                      <Link to={`/levels/${l.id}`}>
+                        {l.name}
+                      </Link>
+                    </li>
+                  )
+                }
+                return null;
+              })}
+            </ul>
+          </div>
+          <div className='world-list'>
+            <h3 id='world-name'>World 2</h3>
+            <ul id='world-2'>
+              {LEVELS.map(l => {
+                if(l.world === 2){
+                  return (
+                    <li key={l.id}>
+                      <Link to={`/levels/${l.id}`}>
+                        {l.name}
+                      </Link>
+                    </li>
+                  )
+                }
+                return null;
+              })}
+            </ul>
+          </div>
+          <div className='world-list'>
+            <h3 id='world-name'>World 3</h3>
+            <ul id='world-3'>
+              {LEVELS.map(l => {
+                if(l.world === 3){
+                  return (
+                    <li key={l.id}>
+                      <Link to={`/levels/${l.id}`}>
+                        {l.name}
+                      </Link>
+                    </li>
+                  )
+                }
+                return null;
+              })}
+            </ul>
+          </div>
+          <div className='world-list'>
+            <h3 id='world-name'>World 4</h3>
+            <ul id='world-4'>
+              {LEVELS.map(l => {
+                if(l.world === 4){
+                  return (
+                    <li key={l.id}>
+                      <Link to={`/levels/${l.id}`}>
+                        {l.name}
+                      </Link>
+                    </li>
+                  )
+                }
+                return null;
+              })}
+            </ul>
+          </div>
+          <div className='world-list'>
+            <h3 id='world-name'>World 5</h3>
+            <ul id='world-5'>
+              {LEVELS.map(l => {
+                if(l.world === 5){
+                  return (
+                    <li key={l.id}>
+                      <Link to={`/levels/${l.id}`}>
+                        {l.name}
+                      </Link>
+                    </li>
+                  )
+                }
+                return null;
+              })}
+            </ul>
+          </div>
+          <div className='world-list'>
+            <h3 id='world-name'>World 6</h3>
+            <ul id='world-6'>
+              {LEVELS.map(l => {
+                if(l.world === 6){
+                  return (
+                    <li key={l.id}>
+                      <Link to={`/levels/${l.id}`}>
+                        {l.name}
+                      </Link>
+                    </li>
+                  )
+                }
+                return null;
+              })}
+            </ul>
+          </div>
+          <div className='world-list'>
+            <h3 id='world-name'>World 7</h3>
+            <ul id='world-7'>
+              {LEVELS.map(l => {
+                if(l.world === 7){
+                  return (
+                    <li key={l.id}>
+                      <Link to={`/levels/${l.id}`}>
+                        {l.name}
+                      </Link>
+                    </li>
+                  )
+                }
+                return null;
+              })}
+            </ul>
+          </div>
+          <div className='world-list'>
+            <h3 id='world-name'>Star World</h3>
+            <ul id='world-8'>
+              {LEVELS.map(l => {
+                if(l.world === 8){
+                  return (
+                    <li key={l.id}>
+                      <Link to={`/levels/${l.id}`}>
+                        {l.name}
+                      </Link>
+                    </li>
+                  )
+                }
+                return null;
+              })}
+            </ul>
+          </div>
+          <div className='world-list'>
+            <h3 id='world-name'>Special World</h3>
+            <ul id='world-9'>
+              {LEVELS.map(l => {
+                if(l.world === 9){
+                  return (
+                    <li key={l.id}>
+                      <Link to={`/levels/${l.id}`}>
+                        {l.name}
+                      </Link>
+                    </li>
+                  )
+                }
+                return null;
+              })}
+            </ul>
+          </div>
+        </div>
       </section>
     )
   }
