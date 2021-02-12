3befab2f4b91591e49ffffcfbed89142fe89193c
Michael Bramble <mike.w.bramble@gmail.com>
2021-02-11 22:39:47 -0500

    uhh my last commit message was false, this time i actually created the component

---
+++ src/Components/GlitchPage/GlitchPage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './glitchpage.css';
+
+export default class GlitchPage extends Component{
+  render(){
+    return (
+      <section className='glitch-container'>
+        <h1>Glitch</h1>
+      </section>
+    )
+  }
+}
